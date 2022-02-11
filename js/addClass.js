451b0e6c325831bbb4870210564f6e2700896053
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2022-02-11 13:07:29 -0300

    feat: Inserindo mais classes pelo JS

--- js/addClass.js
+++ js/addClass.js
@@ -4,7 +4,22 @@ $(document).ready(function() {
         $(this).addClass('card-all') //Classe para controle
     })
     $('#allCards li').each(function() {
-        $(this).addClass('justify-content-center') //Classe para alinhar flex no centro
+        $(this).addClass('justify-content-center d-flex flex-row w-50')
+    })
+    $('li.d-flex > div').each(function() {
+        $(this).addClass('card w-25')
+    })
+    $('div.card img').each(function() {
+        $(this).addClass('card-img-top img-fluid')
+    })
+    $('div.card > div').each(function() {
+        $(this).addClass('card-body')
+    })
+    $('div.card-body h5').each(function() {
+        $(this).addClass('card-title')
+    })
+    $('div.card-body p').each(function() {
+        $(this).addClass('card-text fs-6')
     })
     $('#allCards .card').each(function() {
         $(this).addClass('mx-1') //Margin "nivel" 4
