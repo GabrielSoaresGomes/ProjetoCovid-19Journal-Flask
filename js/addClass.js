d85e4dad60d4ccf9c95521336768e8cbb23730fa
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-02-11 17:20:59 -0300

    style: Melhorando as tabs dos estados

--- js/addClass.js
+++ js/addClass.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
     $('#allCards li').each(function() {
         $(this).addClass('cardContent justify-content-center d-flex flex-row w-50')
     })
+    $('div.card-all h3').each(function() {
+        $(this).addClass('text-uppercase')
+        $(this).css('display', 'inline')
+        $(this).attr('role','button')
+        $(this).addClass('link-primary')
+    })
     $('li.cardContent').each(function(index) {
         if (index == 0) {
             $(this).addClass('tabActive')
@@ -28,6 +34,7 @@ $(document).ready(function() {
     })
     $('div.card-body p').each(function() {
         $(this).addClass('card-text fs-6')
+        $(this).addClass('lead')
     })
     $('#allCards .card').each(function() {
         $(this).addClass('mx-1') //Margin "nivel" 4
