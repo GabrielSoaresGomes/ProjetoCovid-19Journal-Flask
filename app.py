f833bbe8e9899f60ad0b37c23549748d555129c1
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-04-02 16:21:06 -0300

    feat: Melhorando a tela de detelhes de uma noticia

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def error(e):
 @app.route("/")
 def home():
     return render_template("home.html", noticias=lista_noticias, estadosNav=lista_estados,mais_vistas = maisVistas(), mais_curtidas = maisCurtidas(), 
-        mais_recentes = maisRecentes())
+        mais_recentes = maisRecentes(), estados=lista_estados)
 
 
 @app.route("/detalhes/<int:id>")
