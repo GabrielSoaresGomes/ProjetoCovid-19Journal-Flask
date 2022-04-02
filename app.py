3e400457f0c92fb2cc0e9062af5532abe70bc2c0
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-04-02 11:33:58 -0300

    feat: Melhorando Nav e template de error

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ from listas_python.lista_estados import lista_estados
 
 app = Flask(__name__)
 
+@app.errorhandler(404) 
+def error(e): 
+  return render_template("error.html", error=True) 
+
 
 @app.route("/")
 def home():
-    try:
-        return render_template("home.html", noticias=lista_noticias, estados=lista_estados, home=True)
-    except:
-        return render_template("error.html")
+    return render_template("home.html", noticias=lista_noticias, estadosNav=lista_estados)
 
 
 @app.route("/detalhes/<int:id>")
 def detalhes(id):
     for noticia in lista_noticias:
         if int(noticia.get_id()) == int(id):
-            return render_template("detalhes.html", noticia=noticia)
-    return render_template("error.html")
+            return render_template("detalhes.html", noticia=noticia, estadosNav=lista_estados, detalhes=True)
 
 
 @app.route("/estado/<estado>")
@@ -28,8 +28,6 @@ def detalharEstado(estado):
     for est in lista_estados:
         if est.get_estado() == estado:
             return render_template("noticia_estado.html", estado=lista_estados[int(est.get_id())],
-                                   noticias=lista_noticias)
-
-    return render_template("error.html")
+                                   noticias=lista_noticias, estadosNav=lista_estados)
 
 # app.run(debug=True)
