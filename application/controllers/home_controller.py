559cc9b7eadaca340adea85c3221f969002a04db
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-05-30 17:27:39 -0300

    feat: Melhorando classe NoticiaDAO e iniciando controller da home

--- application/controllers/home_controller.py
+++ application/controllers/home_controller.py
@@ -1,10 +1,11 @@
 from flask import render_template
 from application import app
+from application.models.dao.NoticiaDAO import NoticiaDAO
 
 @app.route("/")
 def home():
-    return render_template("home.html", noticias=lista_noticias, estadosNav=lista_estados,mais_vistas = maisVistas(), mais_curtidas = maisCurtidas(), 
-        mais_recentes = maisRecentes(), estados=lista_estados)
+    return render_template("home.html", noticias=NoticiaDAO().get_noticia_lista, estadosNav=lista_estados,mais_vistas = NoticiaDAO().get_mais_vistas(), mais_curtidas = NoticiaDAO().get_mais_curtidas(), 
+        mais_recentes = NoticiaDAO().get_mais_recentes(), estados=lista_estados)
 
 
 
--- application/models/dao/NoticiaDAO.py
+++ application/models/dao/NoticiaDAO.py
@@ -1,4 +1,5 @@
-from pickle import FALSE
+from datetime import datetime
+
 from application.models.entities.Noticia import Noticia
 from .lista_estados import *
 
@@ -225,4 +226,66 @@ class NoticiaDAO:
                                 noticia.views += 1
                                 return noticia
                 return False
+        
+        def get_mais_curtidas(self):
+                mais_curtidas = []
+                for noticia in self.get_noticia_lista:
+                        if len(mais_curtidas) <= 4:
+                                mais_curtidas.append(noticia)
+                        elif noticia.likes > mais_curtidas[0].likes:
+                                mais_curtidas.insert(0, noticia)
+                        elif noticia.likes > mais_curtidas[1].likes:
+                                mais_curtidas.insert(1, noticia)
+                        elif noticia.likes > mais_curtidas[2].likes:
+                                mais_curtidas.insert(2, noticia)
+                        elif noticia.likes > mais_curtidas[3].likes:
+                                mais_curtidas.insert(3, noticia)
+                        elif noticia.likes > mais_curtidas[4].likes:
+                                mais_curtidas.insert(4, noticia)
+                nova_mais_curtidas = []
+                for c in range(0,4):
+                        nova_mais_curtidas.append(mais_curtidas[c])
+                return nova_mais_curtidas
+        
+        def get_mais_vistas(self):
+                mais_vistas = []
+                for noticia in self.get_noticia_lista:
+                        if len(mais_vistas) <= 4:
+                                mais_vistas.append(noticia)
+                        elif noticia.views > mais_vistas[0].views:
+                                mais_vistas.insert(0, noticia)
+                        elif noticia.views > mais_vistas[1].views:
+                                mais_vistas.insert(1, noticia)
+                        elif noticia.views > mais_vistas[2].views:
+                                mais_vistas.insert(2, noticia)
+                        elif noticia.views > mais_vistas[3].views:
+                                mais_vistas.insert(3, noticia)
+                        elif noticia.views > mais_vistas[4].views:
+                                mais_vistas.insert(4, noticia)
+                nova_mais_vistas = []
+                for c in range(0,4):
+                        nova_mais_vistas.append(mais_vistas[c])
+                return nova_mais_vistas
+
+        def get_mais_recentes(self):
+                mais_recentes = []
+                for noticia in self.get_noticia_lista:
+                        data = datetime.strptime(noticia.get_dataPostagem(), "%d/%m/%Y")
+                        if len(mais_recentes) <= 4:
+                                mais_recentes.append(noticia)
+                        elif data > datetime.strptime(mais_recentes[0].get_dataPostagem(), "%d/%m/%Y"):
+                                mais_recentes.insert(0, noticia)
+                        elif data > datetime.strptime(mais_recentes[1].get_dataPostagem(), "%d/%m/%Y"):
+                                mais_recentes.insert(1, noticia)
+                        elif data > datetime.strptime(mais_recentes[2].get_dataPostagem(), "%d/%m/%Y"):
+                                mais_recentes.insert(2, noticia)
+                        elif data > datetime.strptime(mais_recentes[3].get_dataPostagem(), "%d/%m/%Y"):
+                                mais_recentes.insert(3, noticia)
+                        elif data > datetime.strptime( mais_recentes[4].get_dataPostagem(), "%d/%m/%Y"):
+                                mais_recentes.insert(4, noticia)
+                nova_mais_recentes = []
+                for c in range(0,4):
+                        nova_mais_recentes.append(mais_recentes[c])
+                return nova_mais_recentes
+
 
