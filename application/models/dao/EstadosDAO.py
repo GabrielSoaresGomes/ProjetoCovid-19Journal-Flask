7e31edf0d955a45b96939431e19c66401061ce4f
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-05-31 16:13:27 -0300

    feat: Criando a classe EstadoDAO

---
+++ application/models/dao/EstadosDAO.py
@@ -0,0 +1,28 @@
+from application.models.dao import dict_estados
+
+class EstadoDAO:
+    def __init__(self):
+        self.dict_estados = dict_estados
+    
+    def get_lista_estados(self):
+        return self.dict_estados.values()
+
+    def get_por_id(self,id_estado):
+        for estado in self.get_lista_estados():
+            if estado.get_id() == id_estado:
+                return estado
+        return None
+    
+    def get_por_sigla(self, sigla_estado):
+        for estado in self.get_lista_estados():
+            if estado.get_sigla() == sigla_estado:
+                return estado
+        return None
+
+    def get_por_nome(self, nome_estado):
+        for estado in self.get_lista_estados():
+            if estado.get_estado() == nome_estado:
+                return estado
+        return None
+
+
